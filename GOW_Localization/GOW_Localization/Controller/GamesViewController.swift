77b3d3dc953b38e2e04679eb5bfa592fe349de53
EmilioH07 <herreraemilio459@gmail.com>
2024-09-02 14:04:48 -0600

    Agrega icono, colores, page control, igualdad en dark y light, loop infinito con imagen, frances, italiano, espanol e ingles.

--- GOW_Localization/GOW_Localization/Controller/GamesViewController.swift
+++ GOW_Localization/GOW_Localization/Controller/GamesViewController.swift
@@ -12,49 +12,55 @@ class GamesViewController: UIViewController {
     @IBOutlet weak var gamePosterPageControl: UIPageControl!
     @IBOutlet weak var gamePosterImage: UIImageView!
     let gamePosters = Array(0...7)
-    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         gamePosterPageControl.numberOfPages = gamePosters.count
         gamePosterImage.image = UIImage(named: String(gamePosters.first!))
+                
+        // Establecer color del tint a rojo
+        gamePosterPageControl.currentPageIndicatorTintColor = .red
+        gamePosterPageControl.pageIndicatorTintColor = .lightGray
+        
+        updatePageControlImages()
     }
     
 
+    
+
     @IBAction func leftSwipeDone(_ sender: Any) {
-        print("Right")
-        if gamePosterPageControl.currentPage == gamePosters.count-1  {
-                    gamePosterPageControl.currentPage = 0
-                    gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
-                }
-                else{
-                    gamePosterPageControl.currentPage = gamePosterPageControl.currentPage + 1
-                    gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
-                }
-        
+        if gamePosterPageControl.currentPage == gamePosters.count - 1  {
+                   gamePosterPageControl.currentPage = 0
+               } else {
+                   gamePosterPageControl.currentPage += 1
+               }
+               
+               gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+               updatePageControlImages()
     }
     @IBAction func rightSwipeDone(_ sender: Any) {
-        print("Left")
-        
-        
         if gamePosterPageControl.currentPage == 0  {
-                    gamePosterPageControl.currentPage = gamePosters.count-1
-                    gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+                    gamePosterPageControl.currentPage = gamePosters.count - 1
+                } else {
+                    gamePosterPageControl.currentPage -= 1
                 }
-                else{
-                    gamePosterPageControl.currentPage = gamePosterPageControl.currentPage - 1
-                    gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+                
+                gamePosterImage.image = UIImage(named: String(gamePosters[gamePosterPageControl.currentPage]))
+                updatePageControlImages()
+    }
+    
+    func updatePageControlImages() {
+            if #available(iOS 14.0, *) {
+                for index in 0..<gamePosters.count {
+                    if index == gamePosterPageControl.currentPage {
+                        let redLogoImage = UIImage(named: "gow_logo")?.withRenderingMode(.alwaysOriginal)
+                        gamePosterPageControl.setIndicatorImage(redLogoImage, forPage: index)
+                    } else {
+                        let unselectedImage = UIImage(named: "unselectedImage")?.withRenderingMode(.alwaysOriginal)
+                        gamePosterPageControl.setIndicatorImage(unselectedImage, forPage: index)
+                    }
                 }
-    }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+            }
+        }
 }
